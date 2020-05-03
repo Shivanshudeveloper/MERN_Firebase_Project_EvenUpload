316a1ea04d577dd774f81471233d070a35ee1f46
Shivanshu981 <developer.shivanshu@gmail.com>
2020-05-03 11:47:41 -0700

    Line Brake added for Mobile Menu View

--- client/src/components/MobileView/Menu.js
+++ client/src/components/MobileView/Menu.js
@@ -15,24 +15,28 @@ const Menu = () => {
             <div className="four wide column">
                 <Link to="/home" className="item">
                     <i className="large cloud blue icon"></i>
+                    <br />
                     Files
                 </Link>
             </div>
             <div className="four wide column">
                 <Link to="#!" className="item">
                     <i className="large folder yellow icon"></i>
+                    <br />
                     Group
                 </Link>
             </div>
             <div className="four wide column">
                 <Link to="/photos" className="item">
                     <i className="large file purple image icon"></i>
+                    <br />
                     Photos
                 </Link>
             </div>
             <div className="four wide column">
                 <Link to="/scan" className="item">
                     <i className="large folder black qrcode icon"></i>
+                    <br />
                     Scan
                 </Link>
             </div>
