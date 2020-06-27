881828686c64ec192dfa90a82013d6eff4498e65
Shivanshu Gupta <40379768+Shivanshudeveloper@users.noreply.github.com>
2020-06-27 09:08:00 -0700

    Bug with wrong rendering solved

--- client/src/App.js
+++ client/src/App.js
@@ -83,7 +83,7 @@ function App() {
           <Router>
             <Route path="/" exact component={AuthenticationPage} />
             <Route path="/register" exact component={Register} />
-            <Route path="/home" exact component={MobileEvenUpload} />
+            <Route path="/home" exact component={EvenUpload} />
             <Route path="/fileinfo" component={FileInfo} />
             <Route path="/scan" exact component={Scan} />
             <Route path="/photos" exact component={Photos} />
